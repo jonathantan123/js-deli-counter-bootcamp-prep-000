2233c3d35828aed08fcfe6df5d1d8955fc1abb99
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-06 18:28:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-
-
-
-function takeANumber(katzDeliLine, name) {
-  
+function takeANumber (katzDeliLine,name) {
   katzDeliLine.push(`${name}`) 
-  
-   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
+
+var number = 0
+
+function takeANumber (katzDeliLine) {
+
+  katzDeliLine.push(`${++number}`) 
+  
+  return(`Welcome, you have ticket number ${number}`)
+}
+
 
 function nowServing(katzDeliLine) {
-  
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!";
-  }else{
-  return (`Currently serving ${katzDeliLine.shift()}.`); }
+  if (katzDeliLine.length===0) {
+    return("There is nobody waiting to be served!")
+  }
+  else{
+    return(`Currently serving ${katzDeliLine.shift()}.`)
+  }
 }
 
-
-function currentLine (katzDeliLine) {
-  var newArray = [];
-  var i= 0 ;
+function currentLine(katzDeliLine) {
+  var newArray= []
+  var i= 0
   
-  while (i<katzDeliLine.length) {
-  
-  newArray.push(parseInt(i+1) + ". " + katzDeliLine[i++])
-    
+  while(i<katzDeliLine.length) {
+    newArray.push(parseInt(i+1) + ". " + katzDeliLine[i++])
   }
-  
-  if (i===0) {
-    
-  return "The line is currently empty." }
-  
-  else  
-  
-  return "The line is currently: " + newArray.join(", ")}
-  
-
-  
+  if (i === 0) {
+    return ("The line is currently empty.")
+  }
+  else {
+    return("The line is currently: " + newArray.join(", "))
+  }
+}
